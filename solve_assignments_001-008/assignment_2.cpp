3bad8dfcf3a4fb81885fd7fb00ce439903b01aec
Ziad-saadala <142799460+Ziad-saadala@users.noreply.github.com>
2023-08-22 15:15:04 +0300

    Add files via upload

---
+++ solve_assignments_001-008/assignment_2.cpp
@@ -0,0 +1,11 @@
+/*
+  iostream Header File Content
+  -- cout
+  -- cin
+  -- clog
+  -- cerr
+  -- wcin
+  -- wcout
+  -- wcerr
+  -- wclog
+*/
---
+++ solve_assignments_001-008/assignment_3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+ std::cout<< " Iam Learning Programmin\n With \n Elzero \n Web \n School \n <3 \n " ;
+
+}
