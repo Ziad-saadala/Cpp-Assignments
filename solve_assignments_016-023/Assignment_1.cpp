774f4ff0388686d7ae67517b8a10830fc6633351
Ziad-saadala <142799460+Ziad-saadala@users.noreply.github.com>
2023-08-30 23:41:40 +0300

    Add files via upload
    assignments link : https://elzero.org/cpp-assignments-lesson-16-to-23/

---
+++ solve_assignments_016-023/Assignment_1.cpp
@@ -0,0 +1,20 @@
+// Example
+false // bool => Boolean
+
+'a'   // Char => Character
+
+'9'   //Char => Character 
+
+9       //int => Intger - unsigned int
+
+true    //Bool => Boolean
+
+100     //int => Intger - unsigned int
+
+-10     //int => Intger 
+
+0       //int => Intger - bool => boolean
+
+false   // bool => boolean
+
+10.9    // float - double
